b8af525a26b9a3c77821e496378428eaccb21432
JWu <johnnyw131@gmail.com>
2023-08-24 13:56:49 -0400

    tip calculator express application created

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ app.get('/greeting/:name', (req, res) => {
     res.send(`wow! hello there,` + req.params.name);
 });
 
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    res.send(`Tip base off of total: $` + req.params.total + ` ` + `tip%:`+ req.params.tipPercentage);
+});
 
 
 
